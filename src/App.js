ecf7f175c6b1cf5fda3122b08694fa288c1e6948
Michael Wong <michaelwong1391@gmail.com>
2021-12-16 15:42:20 -0500

    add campus is working with the all campuses table

--- src/App.js
+++ src/App.js
@@ -6,12 +6,20 @@ import {
 } from "react-router-dom";
 
 import Homepage from "./Pages/Homepage";
+import Campus from "./Pages/Campus";
+import Student from "./Pages/Student";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/" element={<Homepage />} />
+      </Switch>
+      <Switch>
+        <Route exact path="/campus" element={<Campus />} />
+      </Switch>
+      <Switch>
+        <Route exact path="/student" element={<Student />} />
       </Switch>
     </Router>
   );
